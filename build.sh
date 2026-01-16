ad4fc556d7bd3aafb0edcf9e3d1bef6760e5f845
jordi <bitcurso3@gmail.com>
2026-01-16 13:16:02 +0100

    Migracion a PostgreSQL y soporte de superusuario

--- build.sh
+++ build.sh
@@ -6,3 +6,12 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if env vars are set
+if [[ -n "$DJANGO_SUPERUSER_USERNAME" && -n "$DJANGO_SUPERUSER_PASSWORD" && -n "$DJANGO_SUPERUSER_EMAIL" ]]; then
+    echo "Creating superuser..."
+    python manage.py createsuperuser \
+        --no-input \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL"
+fi
